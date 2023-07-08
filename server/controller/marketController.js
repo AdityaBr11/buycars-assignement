d3289c5717b8ba7f669a4d844ae0a554600ab2e7
AdityaBr11 <adityaanandpurnea1@gmail.com>
2023-07-08 10:36:54 +0530

    filter/sorting/searching added

--- server/controller/marketController.js
+++ server/controller/marketController.js
@@ -3,9 +3,41 @@ const { sendResponse, sendError, sendResponseMsg } = require("../utils/responseH
 
 //get all the market
 exports.getMarket = async (req, res) => {
+    const { q, mileage, price } = req.query;
   try {
-    const market = await MarketModel.find();
-    sendResponse(res, 201, market);
+    if (q) {
+        const market = await MarketModel.find({
+          Model: { $regex: q, $options: "i" },
+        });
+        sendResponse(res, 201, market);
+      } else if (price) {
+        if (price == "asc") {
+          const market = await MarketModel.find().sort({
+            Price: 1,
+          });
+          sendResponse(res, 201, market);
+        }else if (price == "desc") {
+          const market = await MarketModel.find().sort({
+            Price: -1,
+          });
+          sendResponse(res, 201, market);
+        }
+      }else if (mileage) {
+          if (mileage == "asc") {
+            const market = await MarketModel.find().sort({
+              Mileage: 1,
+            });
+            sendResponse(res, 201, market);
+          }else if (mileage == "desc") {
+            const market = await MarketModel.find().sort({
+              Mileage: -1,
+            });
+            sendResponse(res, 201, market);
+          }
+      }else{
+          const market = await MarketModel.find();
+          sendResponse(res, 201, market);
+      }
   } catch (err) {
     sendError(res, 500, err);
   }
--- server/controller/oemController.js
+++ server/controller/oemController.js
@@ -1,21 +1,52 @@
 const oemsModel = require("../models/oems.model");
 const { sendResponse, sendError } = require("../utils/responseHandle");
 
-
-exports.getOEMS=async (req,res)=>{
-    try{
-        const oems=await oemsModel.find();
-        sendResponse(res,201,oems)
-    }catch(err){
-        sendError(res,500,err)
+exports.getOEMS = async (req, res) => {
+  const { q, mileage, price } = req.query;
+  try {
+    if (q) {
+      const oems = await oemsModel.find({
+        Model: { $regex: q, $options: "i" },
+      });
+      sendResponse(res, 201, oems);
+    } else if (price) {
+      if (price == "asc") {
+        const oems = await oemsModel.find().sort({
+          Price: 1,
+        });
+        sendResponse(res, 201, oems);
+      }else if (price == "desc") {
+        const oems = await oemsModel.find().sort({
+          Price: -1,
+        });
+        sendResponse(res, 201, oems);
+      }
+    }else if (mileage) {
+        if (mileage == "asc") {
+          const oems = await oemsModel.find().sort({
+            Mileage: 1,
+          });
+          sendResponse(res, 201, oems);
+        }else if (mileage == "desc") {
+          const oems = await oemsModel.find().sort({
+            Mileage: -1,
+          });
+          sendResponse(res, 201, oems);
+        }
+    }else{
+        const oems = await oemsModel.find();
+        sendResponse(res, 201, oems);
     }
-}
+  } catch (err) {
+    sendError(res, 500, err);
+  }
+};
 
-exports.postOEMS=async (req,res)=>{
-    try{
-        const oems=await oemsModel.create(req.body);
-        sendResponse(res,201,oems)
-    }catch(err){
-        sendError(res,500,err)
-    }
-}
+exports.postOEMS = async (req, res) => {
+  try {
+    const oems = await oemsModel.create(req.body);
+    sendResponse(res, 201, oems);
+  } catch (err) {
+    sendError(res, 500, err);
+  }
+};
